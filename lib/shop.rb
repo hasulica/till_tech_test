b0b2bb12ca529bf993e464929563a12caa01f10e
hasulica <raduasandei@gmail.com>
2016-01-14 12:03:41 +0000

    Parse shop details from JSON file

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,3 +1,14 @@
+require 'json'
+
 class Shop
  attr_reader :name, :address, :phone
+
+  def initialize(filename)
+    file = File.read(filename)
+    data_hash = JSON.parse(file)
+    @name = data_hash[0]['shopName']
+    @address = data_hash[0]['address']
+    @phone = data_hash[0]['phone']
+  end
+
 end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -1,5 +1,18 @@
 require 'shop'
 
 describe Shop do
-  
+
+  subject(:shop) { described_class.new('hipstercoffee.json') }
+
+  it 'has a shop name' do
+    expect(shop.name).to eq 'The Coffee Connection'
+  end
+
+  it 'has an address' do
+    expect(shop.address).to eq '123 Lakeside Way'
+  end
+
+  it 'has a phone number' do
+    expect(shop.phone).to eq '16503600708'
+  end
 end
